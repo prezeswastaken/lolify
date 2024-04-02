a34adf28216f200dffab23764c8f210b27ad326c
prezeswastaken <kacper.preyzner@studenci.collegiumwitelona.pl>
2024-04-02 20:23:34 +0200

    added log tests

---
+++ tests/Feature/LogTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Champion;
+use App\Models\User;
+
+test('log is created when user likes a champion', function () {
+    $user = User::factory()->create();
+
+    $champion = Champion::factory()->create();
+
+    $response = $this->actingAs($user)->post("/api/champion/like/{$champion->id}");
+
+    $response->assertStatus(200);
+
+    $this->assertDatabaseHas('logs', [
+        'user_id' => $user->id,
+        'text' => "$user->name liked $champion->name",
+    ]);
+
+    $user->delete();
+});
+
+test('log is created when user dislikes a champion', function () {
+    $user = User::factory()->create();
+
+    $champion = Champion::factory()->create();
+
+    $this->actingAs($user)->post("/api/champion/like/{$champion->id}");
+    $response = $this->actingAs($user)->post("/api/champion/dislike/{$champion->id}");
+
+    $response->assertStatus(200);
+
+    $this->assertDatabaseHas('logs', [
+        'user_id' => $user->id,
+        'text' => "$user->name disliked $champion->name",
+    ]);
+
+    $user->delete();
+});
